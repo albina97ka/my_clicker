adeeea1dc3499c2b368dee38b8c47cede886995f
albina97ka <albina97ka@gmail.com>
2023-02-25 11:25:14 +0300

    при повороте данные не сбрасываются

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,54 +9,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "StartActivity";
     private Integer count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onCreate");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-    }
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart");
     }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("count", count);
-        Log.d(TAG, "onSaveInstanceState");
     }
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        if (savedInstanceState != null &&
-                savedInstanceState.containsKey("count")) {
+        if (savedInstanceState != null && savedInstanceState.containsKey("count")) {
             count = savedInstanceState.getInt("count");
         }
-        Log.d(TAG, "onRestoreInstanceState");
-    }
-    private void resetUI() {
-        ((TextView) findViewById(R.id.txt_counter)).setText(count.toString());
-        Log.d(TAG, "resetUI");
     }
     public void onClickBtnAddfrogs(View view){
         count++;
